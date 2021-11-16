6f8e179b1ba21c08face189f38cf637fc963f2cd
Liam Bartsch <bartschl@tcd.ie>
2021-11-16 07:23:14 +0000

    Updated readme and python script with functions

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CSU44061_Team_Project
 This is the repository for our Machine Learning Project for the module CS7CS4/CSU44061 Machine Learning
 
-## How to make a commit using the makefile (assuming you have correct access rights and **make** working)
+## How to make a commit using the makefile (assuming you have correct access rights and **make** is installed and working)
 ```make git m="Commit Message"```
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-print('Program Starts')
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import PolynomialFeatures
@@ -15,16 +12,21 @@ from sklearn.model_selection import cross_val_score, train_test_split
 import warnings
 warnings.filterwarnings('ignore')
 
+# Main function starts here ->
+def main():
+    print("Program has started...")
+    pre_processing()
+    print("Program has finished...")
+    return
+
+# Preprocessing function starts here
+def pre_processing():
+    print("Started preprocessing of data...")
+    # Load Data from CSV file of dataset 1
+    weather_dataframe = pd.read_csv('weather.csv')
+    # data = weather_dataframe.values.tolist()
+    print("Finished preprocessing data...")
 
 
-
-
-# MAIN FUCTION STARTS HERE ->
-
-# Load Data from CSV file of dataset 1
-weather_dataframe = pd.read_csv('weather.csv')
-# data = weather_dataframe.values.tolist()
-
-
-# FOR PART 1
-print("Program Ends")
+if __name__ == "__main__":
+    main()
